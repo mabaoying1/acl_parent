1804f2d23b8177dea416f73d56fc9c8215fa42c3
Administrator <780570471@qq.com>
2021-01-14 18:53:39 +0800

    springsecrity权限微服务模块

---
+++ common/service_base/src/main/java/com/atguigu/utils/SwaggerWebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.atguigu.utils;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class SwaggerWebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//        System.out.println("SwaggerWebMvcConfig  addResourceHandlers"  );
+    }
+}
